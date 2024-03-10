0689c383a342f2730eb513694099beea121319ef
yosuva4 <yosuvaitech@gmail.com>
2024-03-11 00:45:11 +0530

    STATCI

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -170,13 +171,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR,"static")
 ]
 
+STAIC_ROOT = os.path.join(BASE_DIR,"static")
 MEDIA_ROOT = 'static/images'
 
 # Default primary key field type
